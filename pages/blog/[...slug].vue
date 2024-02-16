fc3752ae7a169b050c636247322daed61b745f76
tiger <redkross_awesome@hotmail.com>
2024-02-16 01:02:56 +0100

    meta experiment

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,7 +1,9 @@
 <template>
   <article class="w-full flex items-center justify-center flex-col">
     <div class="md:w-2/3 w-screen">
-      <PostTitle :date="post.date" :category="post.category">{{ post.title }}</PostTitle>
+      <PostTitle :date="post.date" :category="post.category">{{
+        post.title
+      }}</PostTitle>
       <div class="text-neutral p-5">
         <ContentRenderer :value="post" class="space-y-2" />
       </div>
@@ -10,7 +12,12 @@
 </template>
 
 <script setup lang="ts">
-  import {useRoute} from "vue-router";
+import { useRoute } from "vue-router";
 
-  const post = await queryContent(useRoute().path).findOne();
+const post = await queryContent(useRoute().path).findOne();
+
+useHead({
+  title: post.title,
+  meta: [{ name: "description", content: post.description }],
+});
 </script>
