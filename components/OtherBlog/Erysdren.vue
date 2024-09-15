7c7486dbfd5ea235ff1a985833daa89a63f7c861
tiger <tiger@kittycat.homes>
2024-09-15 23:41:40 +0200

    ooops the myno font was wrong

--- components/OtherBlog/Erysdren.vue
+++ components/OtherBlog/Erysdren.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
     <div class="flex justify-center bg-[#222] p-2 w-fit rounded-xl">
-    <NuxtLink external target="_blank" to="https://erysdren.me" class="text-2xl font-erysdren bg-[#eeeeee] text-[#222222] flex justify-center px-4 py-2 hover:bg-[#222222] hover:text-[#eeeeee] w-fit border-[#eeeeee] border-dashed border-2">
+    <NuxtLink external target="_blank" to="https://erysdren.me" class="text-3xl font-erysdren bg-[#eeeeee] text-[#222222] flex justify-center px-4 py-2 hover:bg-[#222222] hover:text-[#eeeeee] w-fit border-[#eeeeee] border-dashed border-2">
         erysdren's WWW site
     </NuxtLink>
     </div>
--- components/OtherBlog/Myno.vue
+++ components/OtherBlog/Myno.vue
@@ -3,7 +3,7 @@
     <NuxtLink
       target="_blank"
       to="https://zatzhing.me/"
-      class="link text-4xl p-5 w-2/3 flex gap-2 items-baseline"
+      class="link text-4xl p-5 w-2/3 flex gap-2 items-baseline justify-center"
       ><img class="h-12" src="/images/other-blogs/myno_amused_blep.png" />
       <span>Myno!</span></NuxtLink
     >
@@ -16,7 +16,6 @@
   border-radius: 20px;
   background-color: #3a205f;
   color: white;
-  font-family: "DM Sans", sans-serif;
   text-align: center;
   font-weight: 600;
   text-decoration: none;
