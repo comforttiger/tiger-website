35a89f6fe29613e68d0a007ac259b35ca2075908
tiger <tiger@kittycat.homes>
2024-11-23 12:15:14 +0100

    reorganized buttons

--- components/ButtonsWall.vue
+++ components/ButtonsWall.vue
@@ -3,16 +3,9 @@
     <ul class="flex justify-center flex-wrap gap-1">
       <li>
         <Button
-          img="/buttons/zoe.gif"
-          alt="zoe website"
-          link="https://zoe.kittycat.homes"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/erysdren.png"
-          alt="erysdren's website"
-          link="https://erysdren.me/"
+          img="/buttons/artificialangel.png"
+          alt="444631 / Artificial Angel's website"
+          link="https://444631.xyz/"
         />
       </li>
       <li>
@@ -20,279 +13,6 @@
           img="/buttons/abyss.gif"
           alt="abyss' website"
           link="https://kobold60.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/myno.gif"
-          alt="myno's website"
-          link="https://zatzhing.me"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/oat.png"
-          alt="oat zone"
-          link="https://oat.zone/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/summer.png"
-          alt="summer's website"
-          link="https://summergeist.art/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/tulip.gif"
-          alt="tulip's website"
-          link="https://platinumtulip.garden/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/cecirobin.png"
-          alt="ceci robin's website"
-          link="https://cecirobin.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/indietsushin.gif"
-          alt="インディー通信 Indie Tsushin"
-          link="https://indietsushin.net"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/renkon.gif"
-          alt="renkon's website"
-          link="https://renkotsuban.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/colrana.gif"
-          alt="colrana's website"
-          link="https://colrana.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/citadark.gif"
-          alt="citadark"
-          link="https://citadark.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/pont.gif"
-          alt="pont's website"
-          link="https://pont.cool"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/swiftyshq.gif"
-          alt="swifty's hq"
-          link="https://swiftyshq.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/kokoscript.gif"
-          alt="kokoscript"
-          link="https://kokoscript.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/bnuyy.gif"
-          alt="wordhopper"
-          link="https://hare.kokoscript.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/maebubsy.gif"
-          alt="mae bubsy website"
-          link="https://mae.bubsy.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/tally.gif"
-          alt="coffee spills by tallywinkle"
-          link="https://tallywinkle.bearblog.dev"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/lilyv.gif"
-          alt="lily v's website"
-          link="https://itsmelilyv.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/arborelia.webp"
-          alt="arborelia's website"
-          link="https://posts.arborelia.net/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/quail.png"
-          alt="curiousquail's website"
-          link="https://blog.curiousquail.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/damien.png"
-          alt="damien's website"
-          link="https://damien.zone/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/moxie.gif"
-          alt="moxie's website"
-          link="https://moxiecat.dev/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/bumblebeecity.gif"
-          alt="bumblebee city"
-          link="https://bumblebee.city/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/tuxedodragon.gif"
-          alt="tuxedo dragon's website"
-          link="https://tuxedodragon.art/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/pebble.gif"
-          alt="pebble's website"
-          link="https://pebble.pet/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/beeps.png"
-          alt="beeps website"
-          link="https://beeps.website"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/meltknuckles.gif"
-          alt="meltknuckles website"
-          link="https://meltknuckles.net/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/mooeena.png"
-          alt="mooeena's website"
-          link="https://mooeena.site/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/keeri.gif"
-          alt="keerifox's website"
-          link="https://keeri.place/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/jkap.gif"
-          alt="jkap's website"
-          link="https://jkap.io/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/wowperfect.png"
-          alt="wowperfect.net"
-          link="https://wowperfect.net/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/rubygay.gif"
-          alt="ruby.gay"
-          link="https://ruby.gay/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/dispatchblog.gif"
-          alt="dispatch from the radical dreamland"
-          link="https://blog.radicaldream.land/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/curilagann.gif"
-          alt="curi lagann AKA hedgemom's website"
-          link="https://curilagann.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/alyaza.gif"
-          alt="birzeblog"
-          link="https://alyaza.neocities.org/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/candiedreptile.gif"
-          alt="candiedreptile.club"
-          link="https://candiedreptile.club/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/millie-squilly.png"
-          alt="millie squilly dot com"
-          link="https://milliesquilly.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/zine-of-millie.png"
-          alt="zine of millie"
-          link="https://zine.milliesquilly.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/surasshu.gif"
-          alt="surasshu's website"
-          link="https://surasshu.com/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/meowgarden.png"
-          alt="meow.garden"
-          link="https://meow.garden/"
-        />
-      </li>
-      <li>
-        <Button
-          img="/buttons/blinkiescafe.gif"
-          alt="blinkies.cafe"
-          link="https://blinkies.cafe/"
         />
       </li>
       <li>
@@ -316,13 +36,6 @@
       </li>
       <li>
         <Button
-          img="/buttons/tulipbunny.png"
-          alt="irina's burrow"
-          link="https://tulipbunny.au/"
-        />
-      </li>
-      <li>
-        <Button
           img="/buttons/antdotcomputer.png"
           alt="ant dot computer"
           link="https://ant.computer/"
@@ -330,16 +43,55 @@
       </li>
       <li>
         <Button
-          img="/buttons/artificialangel.png"
-          alt="444631 / Artificial Angel's website"
-          link="https://444631.xyz/"
+          img="/buttons/arborelia.webp"
+          alt="arborelia's website"
+          link="https://posts.arborelia.net/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/beeps.png"
+          alt="beeps website"
+          link="https://beeps.website"
         />
       </li>
       <li>
         <Button
-          img="/buttons/lyra.png"
-          alt="lyra dot horse"
-          link="https://lyra.horse"
+          img="/buttons/alyaza.gif"
+          alt="birzeblog"
+          link="https://alyaza.neocities.org/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/blinkiescafe.gif"
+          alt="blinkies.cafe"
+          link="https://blinkies.cafe/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/bumblebeecity.gif"
+          alt="bumblebee city"
+          link="https://bumblebee.city/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/candiedreptile.gif"
+          alt="candiedreptile.club"
+          link="https://candiedreptile.club/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/cecirobin.png"
+          alt="ceci robin's website"
+          link="https://cecirobin.neocities.org/"
         />
       </li>
       <li>
@@ -351,16 +103,112 @@
       </li>
       <li>
         <Button
+          img="/buttons/citadark.gif"
+          alt="citadark"
+          link="https://citadark.neocities.org/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/colrana.gif"
+          alt="colrana's website"
+          link="https://colrana.neocities.org/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/tally.gif"
+          alt="coffee spills by tallywinkle"
+          link="https://tallywinkle.bearblog.dev"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/curilagann.gif"
+          alt="curi lagann AKA hedgemom's website"
+          link="https://curilagann.neocities.org/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/quail.png"
+          alt="curiousquail's website"
+          link="https://blog.curiousquail.com/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/damien.png"
+          alt="damien's website"
+          link="https://damien.zone/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/dispatchblog.gif"
+          alt="dispatch from the radical dreamland"
+          link="https://blog.radicaldream.land/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/erysdren.png"
+          alt="erysdren's website"
+          link="https://erysdren.me/"
+        />
+      </li>
+      <li>
+        <Button
           img="/buttons/elysia.png"
           alt="elysia's website"
           link="https://le.alphamethyl.barr0w.net/~elysia/"
         />
       </li>
+
+
       <li>
         <Button
           img="/buttons/ghoulnoise.gif"
           alt="ghoulnoise's website"
           link="https://ghoulnoise.com/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/indietsushin.gif"
+          alt="インディー通信 Indie Tsushin"
+          link="https://indietsushin.net"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/tulipbunny.png"
+          alt="irina's burrow"
+          link="https://tulipbunny.au/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/jkap.gif"
+          alt="jkap's website"
+          link="https://jkap.io/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/keeri.gif"
+          alt="keerifox's website"
+          link="https://keeri.place/"
         />
       </li>
       <li>
@@ -370,23 +218,200 @@
           link="https://alloyed.me/"
         />
       </li>
+      <li>
+        <Button
+          img="/buttons/kokoscript.gif"
+          alt="kokoscript"
+          link="https://kokoscript.com/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/lilyv.gif"
+          alt="lily v's website"
+          link="https://itsmelilyv.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/lyra.png"
+          alt="lyra dot horse"
+          link="https://lyra.horse"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/maebubsy.gif"
+          alt="mae bubsy website"
+          link="https://mae.bubsy.org/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/meltknuckles.gif"
+          alt="meltknuckles website"
+          link="https://meltknuckles.net/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/meowgarden.png"
+          alt="meow.garden"
+          link="https://meow.garden/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/millie-squilly.png"
+          alt="millie squilly dot com"
+          link="https://milliesquilly.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/mooeena.png"
+          alt="mooeena's website"
+          link="https://mooeena.site/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/moxie.gif"
+          alt="moxie's website"
+          link="https://moxiecat.dev/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/myno.gif"
+          alt="myno's website"
+          link="https://zatzhing.me"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/oat.png"
+          alt="oat zone"
+          link="https://oat.zone/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/pebble.gif"
+          alt="pebble's website"
+          link="https://pebble.pet/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/pont.gif"
+          alt="pont's website"
+          link="https://pont.cool"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/renkon.gif"
+          alt="renkon's website"
+          link="https://renkotsuban.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/rubygay.gif"
+          alt="ruby.gay"
+          link="https://ruby.gay/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/summer.png"
+          alt="summer's website"
+          link="https://summergeist.art/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/surasshu.gif"
+          alt="surasshu's website"
+          link="https://surasshu.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/swiftyshq.gif"
+          alt="swifty's hq"
+          link="https://swiftyshq.neocities.org/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/tulip.gif"
+          alt="tulip's website"
+          link="https://platinumtulip.garden/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/tuxedodragon.gif"
+          alt="tuxedo dragon's website"
+          link="https://tuxedodragon.art/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/bnuyy.gif"
+          alt="wordhopper"
+          link="https://hare.kokoscript.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/wowperfect.png"
+          alt="wowperfect.net"
+          link="https://wowperfect.net/"
+        />
+      </li>
+
+
+      <li>
+        <Button
+          img="/buttons/zine-of-millie.png"
+          alt="zine of millie"
+          link="https://zine.milliesquilly.com/"
+        />
+      </li>
+      <li>
+        <Button
+          img="/buttons/zoe.gif"
+          alt="zoe website"
+          link="https://zoe.kittycat.homes"
+        />
+      </li>
     </ul>
     <div class="items-center text-center flex flex-col justify-center gap-2">
-      and here are my buttons!
-      <div class="flex">
+      and here's my button!
       <Button 
         img="/tiger-button.gif"
         alt="tiger's website"
         link="https://comforttiger.space/"
         />
-      <Button
-        img="/tiger.png"
-        alt="tiger's website"
-        link="https://comforttiger.space/"
-      />
-      </div>
-
-      <span>second one made by <NuxtLink to="https://zoe.kittycat.homes" class="underline text-secondary inline" target="_blank">zoe</NuxtLink>!</span>
     </div>
   </div>
 </template>
