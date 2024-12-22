93805a7ea4a74ac0b24b7c265aadb2178af66876
tiger <tiger@kittycat.homes>
2024-12-22 03:21:03 +0100

    uhh sure

--- components/RecentMovies.vue
+++ components/RecentMovies.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-base-100 rounded-xl p-5 flex flex-col gap-5">
 <h2 class="text-primary text-4xl font-display text-center"><NuxtLink to="/watching">recently watched</NuxtLink></h2>
-<div class="flex gap-4">
+<div class="flex gap-2">
     <ContentList :query="query" v-slot="{ list }">
         <NuxtLink v-for="movie in list" :to="`/watching#${movie._path?.split('/')[2]}`" :key="movie._path">
           <img :src="movie.image" />
@@ -16,6 +16,6 @@ import type { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
 const query: QueryBuilderParams = {
   path: "/watching",
   sort: [{ timestamp: -1 }],
-  limit: 4
+  limit: 3
 };
 </script>
